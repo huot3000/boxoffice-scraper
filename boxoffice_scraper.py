870063ef681343abf414786aab293352fb91b3de
Philippe Huot <philippe.ht@gmail.com>
2020-07-07 02:11:31 -0400

    Added url_to_file function

--- boxoffice_scraper.py
+++ boxoffice_scraper.py
@@ -2,14 +2,22 @@
 
 # Box-office scraper
 
+import datetime
 import requests
+
+now = datetime.datetime.now()
+year = now.year
+
 
 def url_to_file(url, filename='world.html'):
     r = requests.get(url)
     if r.status_code == 200:
-        hmtl_text = r.text
+        html_text = r.text
+        with open(f'world-{year}.html', 'w') as f:
+            f.write(html_text)
+        return html_text
+    return ''
 
 url='https://www.boxofficemojo.com/year/world/'
 
-
-print(r.status_code)
+url_to_file(url)
